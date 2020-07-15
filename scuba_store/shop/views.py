a3f7269d4ee47564d05e18682b717fe62185f666
tiagoperes <tiagomartinsperes@gmail.com>
2020-07-15 11:13:55 +0100

    Categories views and HTML Template

--- scuba_store/shop/views.py
+++ scuba_store/shop/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Category,Product
 
 def index(request):
     text_var = 'This is a Django app web page.'
     return HttpResponse(text_var)
+
+#Category view
+
+def allProdCat(request, c_slug=None):
+    c_page = None
+    products = None
+    if c_slug!=None:
+        c_page = get_object_or_404(Category,slug=c_slug)
+        products = Product.objects.filter(category=c_page,available=True)
+    else:
+        products = Product.objects.all().filter(available=True)
+    return render(request,'shop/category.html',{'category':c_page,'products':products})
